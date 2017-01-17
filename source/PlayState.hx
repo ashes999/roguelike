bc9fc936b66e405fc5b9235b85352ea872c6746a
ashes999 <ashes999@yahoo.com>
2017-01-17 18:11:19 -0500

    FPS test On dev VM, 4-5 FPS

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,6 +9,9 @@ class PlayState extends FlxState
 {
 	private var texts:Array<Array<FlxText>>;
 	private static inline var FONT_SIZE:Int = 24;
+	
+	private var draws:Int = 0;
+	private var lastFpsCheck = Date.now().getTime();
 
 	override public function create():Void
 	{
@@ -26,5 +29,48 @@ class PlayState extends FlxState
 		text.setFormat("assets/Inconsolata_Regular.ttf", FONT_SIZE, FlxColor.WHITE);
 		add(text);
 		return text;
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		this.draws++;
+		
+		// Redraw everything
+		var validCharacters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-=+";
+
+		for (row in this.texts)
+		{
+			for (text in row)
+			{
+				var len:Int = text.text.length;
+				for (i in 0 ... len)
+				{
+					var r = Std.int(Math.random() * validCharacters.length);
+					var char = validCharacters.charAt(r);
+					text.text = setChar(text.text, i, char);
+				}
+			}
+		}	
+
+		super.update(elapsed);
+
+		var now = Date.now().getTime();
+		var t = now - this.lastFpsCheck;
+		if (t >= 1000)
+		{
+			t = t / 1000.0;
+			var fps:Int = Std.int(this.draws / t);
+			this.lastFpsCheck = now;			
+			this.draws = 0;
+			trace('${fps} FPS');
+		}
+
+	}
+
+	private function setChar(str:String, pos:Int, char:String):String
+	{
+		var pre = str.substring(0, pos);
+		var post = str.substring(pos + 1);
+		return '${pre}${char}${post}';
 	}
 }
