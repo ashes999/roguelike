79a31fbb80a2ae4140e97b8105d4fc740e356460
ashes999 <ashes999@yahoo.com>
2017-01-18 01:19:44 -0500

    One text per row: 20FPS

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,18 +7,27 @@ import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
-	private var texts:Array<Array<FlxText>>;
+	//private var texts:Array<Array<FlxText>>;
+	private var texts:Array<FlxText>;
 	private static inline var FONT_SIZE:Int = 24;
 	
 	private var draws:Int = 0;
 	private var lastFpsCheck = Date.now().getTime();
 
+
+	private var columns:Int = 0;
+
 	override public function create():Void
 	{
 		super.create();
-		var columns:Int = Math.floor(Main.GAME_WIDTH / (FONT_SIZE / 2)); // X
+		this.columns = Math.floor(Main.GAME_WIDTH / (FONT_SIZE / 2)); // X
 		var rows:Int = Math.floor(Main.GAME_HEIGHT / FONT_SIZE); // Y
-		this.texts = [for (x in 0...columns) [for (y in 0...rows) createText(x, y, ".")]];
+		//this.texts = [for (x in 0...columns) [for (y in 0...rows) createText(x, y, ".")]];
+		this.texts = new Array<FlxText>();
+		for (i in 0 ... columns)
+		{
+			texts.push(createText(0, i, ""));
+		}
 		trace('Created a ${columns}x${rows} grid');
 	}
 	
@@ -37,15 +46,7 @@ class PlayState extends FlxState
 		// Redraw everything
 		var validCharacters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-=+";
 
-		for (row in this.texts)
-		{
-			for (text in row)
-			{
-				var r = Std.int(Math.random() * validCharacters.length);
-				var char = validCharacters.charAt(r);
-				text.text = char;
-			}
-		}	
+		drawOneTextPerRow(validCharacters);
 
 		super.update(elapsed);
 
@@ -59,5 +60,20 @@ class PlayState extends FlxState
 			this.draws = 0;
 			trace('${fps} FPS');
 		}
+	}
+
+	private function drawOneTextPerRow(validCharacters:String):Void
+	{
+		for (row in this.texts)
+		{
+			var s = "";
+			for (i in 0 ... this.columns)
+			{
+				var r = Std.int(Math.random() * validCharacters.length);
+				var char = validCharacters.charAt(r);
+				s += char;
+			}
+			row.text = s;
+		}	
 	}
 }
